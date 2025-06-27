9dc3f793ce2b6532bfde5da08262f2355a80e4a4
Ptr-Brightera-Academy <ptrbrighteraacademy@gmail.com>
2025-06-27 14:03:42 +0000

    Updating images and css

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-  return "Peter Moyfdgo"
+  return render_template('home.html')
+
+
+@app.route("/contact-us")
+def contact_us():
+  return "Contact Us"
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug= True)
-  
+  app.run(host='0.0.0.0', debug=True)
