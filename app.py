4fe7951c82e65740e084103d7c530560e2cd4faa
Ptr-Brightera-Academy <ptrbrighteraacademy@gmail.com>
2025-07-01 13:05:03 +0000

    added dynamic data to job page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from database import get_jobs_from_db
+from flask import Flask, render_template, jsonify
+from database import get_jobs_from_db, get_job_from_db
 
 app = Flask(__name__)
            
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def hello_world():
     JOBS = get_jobs_from_db()
     return render_template('home.html', jobs=JOBS)
+    
+@app.route("/job/<id>")
+def show_job(id):
+    job = get_job_from_db(id)
+    if not job:
+        return "Not Found", 404
+    return render_template('jobpage.html', job=job)
+           
 
 @app.route("/contact-us")
 def contact_us():
--- database.py
+++ database.py
@@ -23,3 +23,12 @@ def get_jobs_from_db():
         for row in result.all():
             jobs.append(dict(row._mapping))
         return(jobs)
+
+def get_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), {"val": id})
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+          return dict(rows[0]._mapping)  
