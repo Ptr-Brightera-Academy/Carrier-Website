cda50870731b8700e49aa8b341a1e391a1135245
Ptr-Brightera-Academy <ptrbrighteraacademy@gmail.com>
2025-07-01 10:40:29 +0000

    added connection to db

---
+++ database.py
@@ -0,0 +1,24 @@
+import os
+from sqlalchemy import create_engine, text
+
+cert_path = os.path.join(os.path.dirname(__file__), 'certs', 'isrgrootx1.pem')
+
+ssl_args = {
+    "ssl": {
+        "ca": cert_path
+    }
+}
+my_secret = os.environ['DB_CONNECTION_STRING']
+engine = create_engine(
+  my_secret,
+    connect_args=ssl_args,
+    echo=True
+)
+
+def get_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs"))
+        jobs = []
+        for row in result.all():
+            jobs.append(dict(row._mapping))
+        return(jobs)
