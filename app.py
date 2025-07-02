4655dee130d5a28b26302b549d3c3b5b83e1f074
Ptr-Brightera-Academy <ptrbrighteraacademy@gmail.com>
2025-07-02 06:14:31 +0000

    made some changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import get_jobs_from_db, get_job_from_db
+from flask import Flask, jsonify, render_template, request
+from database import get_jobs_from_db, get_job_from_db, add_application_to_db
 
 app = Flask(__name__)
            
@@ -15,7 +15,13 @@ def show_job(id):
         return "Not Found", 404
     return render_template('jobpage.html', job=job)
            
-
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+    data = request.form
+    job = get_job_from_db(id)
+    add_application_to_db(id, data)
+    return render_template('application_submitted.html', application=data, job=job)
+    
 @app.route("/contact-us")
 def contact_us():
     return "Contact Us"
